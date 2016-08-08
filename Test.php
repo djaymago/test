35dac418991e24b8bd609a21d6e0ab5ceafc76e3
jarnielcataluna <jarniel.cataluna@searchoptmedia.com>
2016-08-08 19:07:49 +0800

    First commit

---
+++ Test.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Propelrr-AJ
+ * Date: 08/08/16
+ * Time: 7:06 PM
+ */
+
+class Test {
+
+    function __construct()
+    {
+        $this->start();
+    }
+
+    function start()
+    {
+        echo "Good job! Start working now!";
+    }
+}
+
+new Test();
