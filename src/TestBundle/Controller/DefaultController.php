9b17163b2fef71ce91c081a4b0fd68e28ba7118a
jarnielcataluna <jarniel.cataluna@searchoptmedia.com>
2016-11-09 22:10:51 +0800

    Test

---
+++ src/TestBundle/Controller/DefaultController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace TestBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('TestBundle:Default:index.html.twig');
+    }
+
+    public function aboutAction()
+    {
+        return $this->render('TestBundle:Default:index.html.twig');
+    }
+
+    public function companyAction()
+    {
+
+        return $this->render('TestBundle:Default:index.html.twig');
+    }
+
+    public function contactAction()
+    {
+
+        return $this->render('TestBundle:Default:index.html.twig');
+    }
+
+    public function signupAction()
+    {
+        return $this->render('TestBundle:Default:index.html.twig');
+    }
+}
