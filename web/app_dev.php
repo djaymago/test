7a43aa587d373aa589030b95b7f619a2f2e086b9
jarnielcataluna <jarniel.cataluna@searchoptmedia.com>
2016-11-09 22:02:06 +0800

    Test

--- web/app_dev.php
+++ web/app_dev.php
@@ -10,7 +10,7 @@ use Symfony\Component\Debug\Debug;
 
 // This check prevents access to debug front controllers that are deployed by accident to production servers.
 // Feel free to remove this, extend it, or make something more sophisticated.
-use Symfony\Component\HttpFoundation\Request;
+//use Symfony\Component\HttpFoundation\Request;
 
 /** @var \Composer\Autoload\ClassLoader $loader */
 $loader = require __DIR__ . '/../app/autoload.php';
