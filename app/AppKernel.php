da91fe7183353e84a33ac53aa25a049a21672cd4
jarnielcataluna <jarniel.cataluna@searchoptmedia.com>
2016-11-09 21:31:10 +0800

    Test

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             //new Jay\TestBundle\TestBundle(),
+            new Test\TestBundle\TestTestBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
