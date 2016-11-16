a6f54a9702cb69042d1bd31a1dfb13b9dda2df40
jarnielcataluna <jarniel.cataluna@searchoptmedia.com>
2016-11-16 20:01:39 +0800

    Test Email

--- src/TestBundle/Controller/DefaultController.php
+++ src/TestBundle/Controller/DefaultController.php
@@ -8,45 +8,31 @@ ignore_user_abort(1);
 
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\JsonResponse;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
-        $to = "denmark.mago@searchoptmedia.com";
-        $subject = "HTML email";
 
-        $message = "
-        <html>
-        <head>
-        <title>HTML email</title>
-        </head>
-        <body>
-        <p>This email contains HTML Tags!</p>
-        <table>
-        <tr>
-        <th>Firstname</th>
-        <th>Lastname</th>
-        </tr>
-        <tr>
-        <td>John</td>
-        <td>Doe</td>
-        </tr>
-        </table>
-        </body>
-        </html>
-        ";
+        if (isset($_POST['submit'])) {
+            $to=$_POST['to'];
+            $from=$_POST['from'];
+            $subject=$_POST['subject'];
+            $message=$_POST['message'];
+            $head="From: $from" .  "\r\n".
+                'Reply-To: '. $from .  "\r\n";
+            $her=$head.' < '.$from .' >';
 
-// Always set content-type when sending HTML email
-        $headers = "MIME-Version: 1.0" . "\r\n";
-        $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
 
-// More headers
-        $headers .= 'From: <webmaster@example.com>' . "\r\n";
-        $headers .= 'Cc: myboss@example.com' . "\r\n";
+            $ret=mail($to, $subject, $message, $her);
+            if($ret==true){
+                echo "<br /> Mail sent Successfully";
+            }else{
+                echo "<br /> Unable to Send mail.";
+            }
+        }
 
-        return new JsonResponse(array(mail($to,$subject,$message,$headers)?'ok':'failed'));
+        return $this->render('TestBundle:Default:index.html.twig');
     }
 
     public function aboutAction()
