6d3ddd9ee57f99d5a1188a5a2e30e4167bf16c92
jarnielcataluna <jarniel.cataluna@searchoptmedia.com>
2016-11-16 19:57:29 +0800

    Test Email

--- src/TestBundle/Controller/DefaultController.php
+++ src/TestBundle/Controller/DefaultController.php
@@ -14,42 +14,39 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-?>
-        <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-        <html xmlns="http://www.w3.org/1999/xhtml">
+        $to = "denmark.mago@searchoptmedia.com";
+        $subject = "HTML email";
+
+        $message = "
+        <html>
         <head>
-            <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-            <title>TurnkeyForms.com - Fake Mailer</title>
-            <link href="/css/main.css" rel="stylesheet" />
-            <script language="javascript" type="text/javascript" src="/js/main.js"></script>
+        <title>HTML email</title>
         </head>
         <body>
-        <h1>Fake Mailer</h1>
-        <form method="post" action="/">
-
-            <input type="hidden" name="action" value="send_mail" />
-            <label for="email_to">Recipient's Email</label>
-            <input type="text" name="email_to" id="email_to" class="input" /><br />
-            <label for="email_from">Sender's Email</label>
-            <input type="text" name="email_from" id="email_from" class="input" /><br />
-            <label for="email_you">Your Email</label>
-            <input type="text" name="email_you" id="email_you" class="input" /><br />
-            <label for="message_subject">Message Subject</label>
-            <input type="text" name="message_subject" id="message_subject" class="input" /><br />
-            <label for="message_body">Message Body</label>
-            <textarea name="message_body" id="message_body" class="textarea"></textarea><br />
-            <label>&nbsp;</label>
-            <input type="submit" value="Send Mail" class="button" />
-            <input type="reset" value="Reset Form" class="button" />
-            <div class="remaining"></div>
-        </form>
-
-        <div id="footer"><a href="/?p=view">View Sent Messages</a> (requires password)</div>
-        <p><a target="_blank" href="http://www.turnkeyforms.com/">Turnkey Scripts</a></p>
+        <p>This email contains HTML Tags!</p>
+        <table>
+        <tr>
+        <th>Firstname</th>
+        <th>Lastname</th>
+        </tr>
+        <tr>
+        <td>John</td>
+        <td>Doe</td>
+        </tr>
+        </table>
         </body>
         </html>
-    <?php
-        echo json_encode(array('ok'));
+        ";
+
+// Always set content-type when sending HTML email
+        $headers = "MIME-Version: 1.0" . "\r\n";
+        $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+
+// More headers
+        $headers .= 'From: <webmaster@example.com>' . "\r\n";
+        $headers .= 'Cc: myboss@example.com' . "\r\n";
+
+        return new JsonResponse(array(mail($to,$subject,$message,$headers)?'ok':'failed'));
     }
 
     public function aboutAction()
