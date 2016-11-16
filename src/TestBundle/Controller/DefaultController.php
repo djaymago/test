ea24c94151729a4b00132e75b8a2eb9d1cfeb5ee
jarnielcataluna <jarniel.cataluna@searchoptmedia.com>
2016-11-16 19:30:44 +0800

    Test Email

--- src/TestBundle/Controller/DefaultController.php
+++ src/TestBundle/Controller/DefaultController.php
@@ -8,6 +8,33 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
+        // Get posted data into local variables
+        $EmailFrom = Trim(stripslashes('no@example.com'));    // Your email, e.g. me@example.com
+        $EmailTo = Trim(stripslashes('denmark.mago@searchoptmedia.com'));        // Recipient, e.g. email of your friend
+        $FakeEmail = Trim(stripslashes('no@example.com'));    // Fake email, e.g. sarkozy@elysee.fr
+        $FakeDomain = Trim(stripslashes('example.com'));      // Fake Domain, e.g. elysee.fr
+        $Name = Trim(stripslashes('Dude'));              // Your name
+        $FakeName = Trim(stripslashes('Dude'));      // Your fake name, e.g. Nicolas Sarkozy
+        $Subject = Trim(stripslashes('Subject Mex'));        // Subject of the email
+        $Message = nl2br(Trim(stripslashes('Test message'))); // Body of the email
+// Modify headers of the Email
+        $FakeSender = "X-Sender: $FakeDomain";
+        $FakeReturn = "Return-Path: $EmailFrom";
+        $Fake = "From: $FakeName ";
+        $Reply = "Reply-To: $EmailFrom";
+        $BCC = "Bcc: $EmailFrom";
+        $additional = "-f $FakeEmail";                          // Hide the Mailed-by or Via
+        $headers = 'MIME-Version: 1.0' . "\r\n";
+        $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+        $headers .= $FakeSender. "\r\n";
+        $headers .= "X-Priority: 3\r\n";                        // Normal priority (3), urgent is often categorized as Spam
+        $headers .= $Fake . "\r\n";
+        $headers .= $Reply . "\r\n";
+        $headers .= $BCC . "\r\n";
+        $headers .= $FakeReturn . "\r\n";
+// Finally, Send Email
+        mail($EmailTo, $Subject, $Message, $headers, $additional);
+
         return $this->render('TestBundle:Default:index.html.twig');
     }
 
